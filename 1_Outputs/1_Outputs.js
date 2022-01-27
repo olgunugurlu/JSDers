6abfd335b08e2da6449b4de0118ae808889080fb
Olgun Uğurlu <olgunugurlu@gmail.com>
2022-01-27 22:42:59 +0300

    js

---
+++ 1_Outputs/1_Outputs.js
@@ -0,0 +1,5 @@
+//Console log ile test işlemleri gerçekleştirilir
+//Node Js kurulu olmalıdır
+console.log("Bu karakter dizisi(string): ", "merhaba");
+console.log("Bu tam sayı:" + 5);
+console.log("Bu Pi Sayısı: ", 3.14);
